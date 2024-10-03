1f7b0c420edceeefc884a534236d908e24fb1902
kevinpicon01 <digitalpruu@gmail.com>
2024-10-03 16:42:37 -0500

    Fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,36 +1,38 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Variedades Eliana";
+const slogan = "Variedades Eliana: Calidad y bienestar para cada día.";
+const email = "variedadeseliana@example.com"; // Cambia este email al correcto
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría obtener más información. ¡Gracias!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = ""; // Cambia este número al correcto
+
 const textos = {
     index: {
-        title: title,
+        title: `${title}`,
+        slogan: `${slogan}`,
     },
     homeContent: {
-        title: title,
+        title: `${title}`,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En Variedades Eliana, nos dedicamos a ofrecerte los mejores productos de perfumería, cosméticos y artículos de aseo, seleccionados cuidadosamente para satisfacer tus necesidades diarias. Desde fragancias cautivadoras hasta productos de belleza de alta calidad, cada artículo en nuestra tienda ha sido elegido para brindarte bienestar y confianza. Nuestro compromiso es proporcionarte una experiencia de compra única, con productos que te harán sentir y lucir bien todos los días. En Variedades Eliana, la calidad y el servicio son nuestra prioridad, ofreciéndote siempre lo mejor para el cuidado personal."
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "",
+            p2: slogan,
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/variedadeseliana",
+        instagram: "https://www.instagram.com/variedadeseliana",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
+
 export default textos;
